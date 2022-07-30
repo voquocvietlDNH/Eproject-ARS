77b9852468f99af97a217f91efd6e7444758482a
voquocvietlDNH <95222601+voquocvietlDNH@users.noreply.github.com>
2022-07-30 22:05:36 +0700

    ADD: Project DOANSEMIII
    Tạo mới dự án

---
+++ DOANSEMIII/TICKETBOOKING/App_Start/RouteConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TICKETBOOKING
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // mặc định
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
+
+            // Điều hướng đến trang mong muốn\
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+               namespaces: new string[] { "TICKETBOOKING.Areas.Guest.Controllers.HomeController" }
+           ).DataTokens.Add("Area", "Guest");
+        }
+    }
+}
